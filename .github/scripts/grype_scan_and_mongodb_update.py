9a5dad2cf0abc19c3aa9f6845f9a8f3831629d6d
Ratnesh Maurya <85143283+ratnesh-maurya@users.noreply.github.com>
2023-12-10 13:34:33 +0530

    Update grype_scan_and_mongodb_update.py

--- .github/scripts/grype_scan_and_mongodb_update.py
+++ .github/scripts/grype_scan_and_mongodb_update.py
@@ -1,38 +1,24 @@
+# run_grype_and_push_to_mongo.py
+
 import subprocess
-from pymongo import MongoClient
-from datetime import datetime
-import os
+import pymongo
 import json
 
-# Read the image name from the file
-with open("config/image.txt", "r") as file:
-    image_name = file.read().strip()
+# Run Grype and store the output in a variable
+grype_output = subprocess.run(["grype", "alpine:latest", "-o", "json"], capture_output=True, text=True)
 
-# Run Grype scan and capture the JSON output
-grype_output = subprocess.check_output(["grype", "--json", image_name])
-grype_results = json.loads(grype_output.decode("utf-8"))
+# Print Grype output
+print(grype_output.stdout)
 
-# Extract CVE information from Grype results
-cve_list = []
-for package in grype_results.get("artifacts", []):
-    for vulnerability in package.get("vulnerabilities", []):
-        cve_list.append(vulnerability.get("id"))
+# Connect to MongoDB
+client = pymongo.MongoClient("mongodb+srv://ratnesh:ratnesh@cluster0.3ka0uom.mongodb.net/cve_db?retryWrites=true&w=majority")
+db = client.cve_db
+collection = db.cve_list
 
-# Connect to MongoDB using the secret
-mongo_connection_string = os.getenv("MONGODB_CONNECTION_STRING")
-client = MongoClient(mongo_connection_string)
-db = client["cve_db"]
-collection = db["cve_list"]
-
-# Create a document with CVE information
-cve_document = {
-    "image_name": image_name,
-    "cve_list": cve_list,
-    "scan_date": datetime.now()
-}
-
-# Insert the document into MongoDB
-collection.insert_one(cve_document)
-
-# Close MongoDB connection
-client.close()
+# Parse Grype output and insert into MongoDB
+try:
+    cve_data = json.loads(grype_output.stdout)
+    collection.insert_many(cve_data)
+    print("Data inserted into MongoDB successfully.")
+except json.JSONDecodeError as e:
+    print(f"Error parsing Grype output: {e}")
