5ef2f1cb48b100c9d1a489bc9fffd55daede9ba6
Ratnesh Maurya <85143283+ratnesh-maurya@users.noreply.github.com>
2023-12-10 13:44:08 +0530

    Update run_grype_and_push_to_mongo.py

--- .github/scripts/run_grype_and_push_to_mongo.py
+++ .github/scripts/run_grype_and_push_to_mongo.py
@@ -1,4 +1,4 @@
-# ./workflow/script/run_grype_and_push_to_mongo.py
+# .workflow/script/run_grype_and_push_to_mongo.py
 
 import subprocess
 import pymongo
@@ -18,7 +18,17 @@ collection = db.cve_list
 # Parse Grype output and insert into MongoDB
 try:
     cve_data = json.loads(grype_output.stdout)
-    collection.insert_many(cve_data)
+    
+    if isinstance(cve_data, list):
+        # If Grype output is a list, insert each item separately
+        for item in cve_data:
+            collection.insert_one(item)
+    elif isinstance(cve_data, dict):
+        # If Grype output is a dictionary, insert it as a single document
+        collection.insert_one(cve_data)
+
     print("Data inserted into MongoDB successfully.")
 except json.JSONDecodeError as e:
     print(f"Error parsing Grype output: {e}")
+except Exception as e:
+    print(f"Error inserting data into MongoDB: {e}")
