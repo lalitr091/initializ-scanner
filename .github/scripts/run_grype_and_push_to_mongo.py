84deb498c463352c84e81718d510cc79c3dc02b9
Ratnesh Maurya <85143283+ratnesh-maurya@users.noreply.github.com>
2023-12-13 14:46:01 +0530

    Update run_grype_and_push_to_mongo.py

--- .github/scripts/run_grype_and_push_to_mongo.py
+++ .github/scripts/run_grype_and_push_to_mongo.py
@@ -5,7 +5,7 @@ import pymongo
 import json
 
 # Run Grype and store the output in a variable
-grype_output = subprocess.run(["grype", "cgr.dev/chainguard/go", "-o", "json"], capture_output=True, text=True)
+grype_output = subprocess.run(["grype", "public.ecr.aws/t4s8c0c3/go:latest", "-o", "json"], capture_output=True, text=True)
 
 
 # Print Grype output
