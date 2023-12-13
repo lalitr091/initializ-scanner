be7a0aca130544fd86ae68639334469e55b0b5a1
Ratnesh Maurya <85143283+ratnesh-maurya@users.noreply.github.com>
2023-12-13 16:58:57 +0530

    Update run_grype_and_push_to_mongo.py

--- .github/scripts/run_grype_and_push_to_mongo.py
+++ .github/scripts/run_grype_and_push_to_mongo.py
@@ -2,30 +2,39 @@ import subprocess
 import pymongo
 import json
 
-image_name = "public.ecr.aws/initializ/go:latest"
-
+# Read image names from the image.txt file in the config folder
+image_file_path = "config/image.txt"
 try:
-    # Run Grype for the image and store the output in a variable
-    grype_output = subprocess.run(["grype", image_name, "-o", "json"], capture_output=True, text=True, check=True)
+    with open(image_file_path, "r") as file:
+        image_names = [line.strip() for line in file.readlines()]
+except FileNotFoundError:
+    print(f"Error: Image file '{image_file_path}' not found.")
+    exit(1)
 
-    # Print Grype output
-    print("Grype stdout:", grype_output.stdout)
-    print("Grype stderr:", grype_output.stderr)
+# Connect to MongoDB
+client = pymongo.MongoClient("mongodb+srv://ratnesh:ratnesh@cluster0.3ka0uom.mongodb.net/cve_db?retryWrites=true&w=majority")
+db = client.cve_db
+collection = db.cve_list
 
-    # Parse Grype output
-    grype_data = json.loads(grype_output.stdout)
-    matches = grype_data.get("matches", [])
+for image_name in image_names:
+    try:
+        # Run Grype for each image and store the output in a variable
+        grype_output = subprocess.run(["grype", image_name, "-o", "json"], capture_output=True, text=True, check=True)
 
-    if not matches:
-        print(f"No vulnerabilities found in Grype output for {image_name}.")
-    else:
-        print(f"Vulnerabilities found in Grype output for {image_name}.")
-
-except subprocess.CalledProcessError as e:
-    print(f"Error running Grype for {image_name}: {e}")
-    # Print stderr for more details
-    print("Grype stderr:", e.stderr)
-except json.JSONDecodeError as e:
-    print(f"Error parsing Grype output for {image_name}: {e}")
-except Exception as e:
-    print(f"Error for {image_name}: {e}")
+        # Print Grype output
+        print(grype_output.stdout)
+        # Parse Grype output and insert into MongoDB
+        grype_data = json.loads(grype_output.stdout)
+        matches = grype_data.get("matches", [])
+        for match in matches:
+            # Include the image name in the MongoDB document
+            match["image"] = image_name
+            collection.insert_one(match)
+        if not matches:
+            # Insert a message into MongoDB if no vulnerabilities found
+            collection.insert_one({"image": image_name, "message": "No vulnerabilities found."})
+            print(f"No vulnerability matches found in Grype output for {image_name}. Message inserted into MongoDB.")
+        else:
+            print(f"Data inserted into MongoDB for {image_name} successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error running Grype for {image_name}: {e}")
